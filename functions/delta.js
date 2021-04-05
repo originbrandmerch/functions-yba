a78354889aa7b6b2200fd3b3c4ca59bd4959f564
Drew Royster <drew.royster@gmail.com>
2021-04-05 15:55:26 -0600

    bumping up node version

--- functions/delta.js
+++ functions/delta.js
@@ -22,13 +22,14 @@ exports.deltaOrder = functions.pubsub.topic('delta_order').onPublish((message) =
   console.log(body);
   console.log(typeof body.data);
   console.log(body.data);
+  const jsonBody = JSON.parse(body);
   return axios({
     method: 'POST',
     url: 'https://sandbox.dtg2goportal.com/api/v1/workorders',
     headers: {
       apikey: 'AB909D6C79252F0CCBC65870D1B89B40',
     },
-    data: body.data,
+    data: jsonBody.data,
   })
     .then(async ({ data: responseData }) => {
       const res = await pubsub.topic('delta_response').publish(Buffer.from(JSON.stringify({ jobId, jobTypeId, data: responseData })));
